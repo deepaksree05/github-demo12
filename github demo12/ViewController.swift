bfee675cf76e1cd999880e42778829045c8d388a
deepaksree05 <deepaknetaxis@gmail.com>
2023-03-02 11:31:44 +0530

    Initial Commit

---
+++ github demo12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github demo12
+//
+//  Created by Netaxis on 2023-03-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("hii")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
